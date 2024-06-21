01363692aeca2e97ccc36c9ddffd27bbea0ea1ff
Putra Prasetya <putraprasetya333@gmail.com>
2024-06-21 18:29:08 +0700

    Slicing WO List

---
+++ src/pages/wo-list/tabInteractive.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const tabs = document.querySelectorAll('[data-tabs-target]')
+  const tabContents = document.querySelectorAll('[role="tabpanel"]')
+
+  tabs.forEach(tab => {
+    tab.addEventListener('click', function () {
+      const target = document.querySelector(tab.dataset.tabsTarget)
+
+      tabContents.forEach(tc => {
+        tc.classList.add('hidden')
+      })
+
+      tabs.forEach(t => {
+        t.classList.remove('border-blue-500', 'text-blue-500')
+        t.setAttribute('aria-selected', 'false')
+      })
+
+      target.classList.remove('hidden')
+      tab.classList.add('border-blue-500', 'text-blue-500')
+      tab.setAttribute('aria-selected', 'true')
+    })
+  })
+})
